f9ce12774725da0826b64a2d394351d7d6f3ef76
NicoNicoNico123 <98505073+NicoNicoNico123@users.noreply.github.com>
2023-04-28 17:45:36 +0800

    Remove env folder from repository

--- env/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/sing/Python/ml-course/sample_project/env/lib"
-XSLT_LIBS="-lxslt -L/Users/sing/Python/ml-course/sample_project/env/lib -lxml2 "
-XSLT_PRIVATE_LIBS=""
-XSLT_INCLUDEDIR="-I/Users/sing/Python/ml-course/sample_project/env/include"
-MODULE_VERSION="xslt-1.1.37"
